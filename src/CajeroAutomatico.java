0805708fca6d529f96da22db3a02181d76667f28
rebeca poma <mrebecapoma.6@gmail.com>
2025-03-22 12:36:27 +0100

    version2-cajero

--- src/CajeroAutomatico.java
+++ src/CajeroAutomatico.java
@@ -9,8 +9,13 @@ public class CajeroAutomatico {
     private static int totalRetiros = 0;
     private static int numeroIngresos = 0;
     private static int numeroRetiros = 0;
-
-    public static void main(String[] args) throws Exception {
+/**
+ * Metodo fundamental que implementa al cajero automatico
+ * Nos muestra un menu con alternativas hasta que el usuario decide salir.
+ * @param args Argumentos de la linea de comandos
+ *
+ */
+    public static void main(String[] args){
 
         int opcion;
         do {
@@ -28,6 +33,7 @@ public class CajeroAutomatico {
                     break;
                 case 4:
                     mostrarEstadistica();
+                    System.out.println();
                     System.out.println("Gracias por usar el cajero automático.¡Hasta luego!");
                     break;
 
@@ -38,8 +44,10 @@ public class CajeroAutomatico {
         } while (opcion != 4);
 
     }
-
-    public static void mostrarMenu() {
+/**
+ * Nos muestra el menu 
+*/
+    private static void mostrarMenu() {
         System.err.println(" _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
         System.err.println("|     Cajero Automático       |");
         System.err.println("|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _|");
@@ -49,54 +57,75 @@ public class CajeroAutomatico {
         System.err.println("|     4. Salir                |");
         System.err.println("|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _|");
     }
-
-    public static void consultarSaldo() {
+    private static void consultarSaldo() {
         System.out.printf("Su saldo actual es: $%.2f%n", (double) saldo);
 
     }
 
-    public static void depositarDinero() {
-
-        int cantidadDeposito = leerEntero("Ingrese la cantidad a depositar: ");
-        if (cantidadDeposito > 0) {
-            saldo += cantidadDeposito;
-            totalIngresos += cantidadDeposito;
-            numeroIngresos++;
-            System.out.printf("Depósito exitoso. Saldo actualizado: $%.2f%n", (double) saldo);
-        } else {
-            System.out.println("Error: El importe debe ser mayor a 0.");
-        }
-    }
-
-    public static void retirarDinero() {
-        int cantidadRetiro = leerEntero("Ingrese la cantidad a retirar: ");
-
-        if (cantidadRetiro > 0) {
-            if (cantidadRetiro <= saldo) {
-                saldo -= cantidadRetiro;
-                totalRetiros += cantidadRetiro;
-                numeroRetiros++;
-                System.out.printf("Retiro exitoso. Saldo actualizado:  $%.2f%n", (double) saldo);
-
-            } else {
-                System.out.println("Error : Saldo insuficiente");
+    private static int realizarOperaciones(String tipoTransaccion , int cantidad, int saldoActual,int totalTransacciones,int numeroTransacciones){
+        if (cantidad > 0) {
+            if (tipoTransaccion.equals("Deposito")) {
+                saldoActual += cantidad;
+                totalTransacciones += cantidad;
+                numeroTransacciones ++;
+                System.out.printf("Depósito exitoso. Saldo actualizado: $%.2f%n", (double) saldoActual);
+            } else if (tipoTransaccion.equals("Retiro")) {
+                if (cantidad <= saldoActual) {
+                    saldoActual -= cantidad;
+                    totalTransacciones += cantidad;
+                    numeroTransacciones++;
+                    System.out.printf("Retiro exitoso. Saldo actualizado:  $%.2f%n", (double) saldoActual);
+                } else{
+                    System.out.println("Error : Saldo insuficiente");
+                }
+                
             }
-        } else {
+           
+        }else{
             System.out.println("Error: El importe debe ser mayor a 0.");
         }
+        return saldoActual;
     }
 
-    public static void mostrarEstadistica() {
+ 
+/**
+ * @param cantidad la cantidad a depositar debe ser un numero entero
+ * @return void
+ * 
+ */
+    private static void depositarDinero() {
+
+        int cantidad = leerEntero("Ingrese la cantidad a depositar: ");
+        //saldo = realizarOperaciones("Deposito", cantidadDeposito, saldo, totalIngresos,numeroIngresos);
+        saldo = realizarOperaciones("Deposito", cantidad, saldo, totalIngresos,numeroIngresos);
+        totalIngresos += cantidad;
+        numeroIngresos ++;
+    }
+
+    private static void retirarDinero() {
+        //int cantidadRetiro = leerEntero("Ingrese la cantidad a retirar: ");
+        //saldo = realizarOperaciones("Retiro", cantidadRetiro, saldo, totalRetiros, numeroRetiros);
+        //saldo = realizarOperaciones("Retiro", cantidadRetiro, cantidadRetiro, cantidadRetiro, cantidadRetiro, cantidadRetiro);
+       int cantidad = leerEntero("Ingrese la cantidad a retirar: ");
+       saldo = realizarOperaciones("Retiro", cantidad, saldo, totalRetiros, numeroRetiros);
+        totalRetiros += cantidad;
+        numeroRetiros++;
+    }
+
+    private static void mostrarEstadistica() {
         System.out.println("----------------ESTADÍSTICAS DE USO------------------");
-        System.out.printf("Número total de Ingresos: $%d%n", numeroIngresos);
+        System.out.printf("Número total de Ingresos: %d%n", numeroIngresos);
         System.out.printf("Importe total Ingresado: $%.2f%n", (double) totalIngresos);
         System.out.printf("Número total de Retiradas:%d%n", numeroRetiros);
         System.out.printf("Importe total Retirado: $%.2f%n", (double) totalRetiros);
         System.out.printf("Saldo Final en cuenta:$%.2f%n", (double) saldo);
-        System.out.printf("-----------------------------------------------------");
     }
-
-    public static int leerEntero(String mensaje) {
+/**
+ * 
+ * @param mensaje
+ * @return
+ */
+    private static int leerEntero(String mensaje) {
         int numero = -1;
         boolean entradaOk = false;
         while (!entradaOk) {
